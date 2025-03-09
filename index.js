b4b95123cacd20040796a705207ea5b1752f0063
Johnson Alala <jonesalala95@gmail.com>
2025-03-10 01:48:04 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activities = "roller-skate") {
+  return `This Saturday, I want to ${activities}!`;
+}
+console.log(saturdayFun());
+
+function mondayWork(activities = "go to the office") {
+  return `This Monday, I will ${activities}.`;
+}
+console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  }
+}
+console.log(wrapAdjective()("a hard worker"));
+console.log(wrapAdjective("||")("a dedicated programmer"));
